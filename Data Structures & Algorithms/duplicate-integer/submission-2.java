92e01421be46e9466e0a69df4c2095443d0e6a9f
sagarreddy1203 <112106971+sagarreddy1203@users.noreply.github.com>
2026-06-28 11:11:57 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,17 @@
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count()<nums.length;
+    }
+}
