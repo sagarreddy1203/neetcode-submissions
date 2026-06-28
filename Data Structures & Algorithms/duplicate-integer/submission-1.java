e6ab755b494202548bf08d973458c60b94d365ab
sagarreddy1203 <112106971+sagarreddy1203@users.noreply.github.com>
2026-06-28 11:08:04 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,24 @@
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet hs=new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i])){
+                return true;
+            }
+            hs.add(nums[i]);
+        }
+        return false;
+    }
+}
